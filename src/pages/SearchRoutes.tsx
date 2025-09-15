eb0bdbf2bba4c47b0bcdf481e7f6e975d2dfa18d
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-09-15 17:50:36 +0900

    Test: jquery ajax REST API test

--- src/pages/SearchRoutes.tsx
+++ src/pages/SearchRoutes.tsx
@@ -1,9 +1,27 @@
+import $ from "jquery";
+
 import { useState } from "react";
 import { clsx } from "clsx";
 
+interface Route {
+    RTE_ID: string;
+    RTE_NM: string;
+}
+
 export default function SearchRoutes() {
     const [searchInput, setSearchInput] = useState<string>("");
-    const sample_data = [{}];
+    const [routes, setRoutes] = useState<Array<Route>>([]);
+
+    const search: (query: string) => void = (query) => {
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:8080/searchRoute",
+            data: { query },
+            dataType: "json",
+        }).then((res) => {
+            setRoutes(res);
+        });
+    };
 
     return (
         <div className={"flex flex-col items-center"}>
@@ -29,8 +47,13 @@ export default function SearchRoutes() {
                         "ml-3 px-3 font-suite bg-neutral-700 border-neutral-400 border-1 rounded-[5px] h-15 w-20",
                         "cursor-pointer transition-all duration-200 hover:scale-105 hover:shadow-2xl"
                     )}
+                    onClick={() => search(searchInput)}
                 />
             </div>
+            {routes &&
+                routes.map((item: Route, idx: number) => (
+                    <h1 key={idx}>{item["RTE_ID"]}</h1>
+                ))}
         </div>
     );
 }
