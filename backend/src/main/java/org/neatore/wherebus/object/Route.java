a566491291180c4b96fe85a9456c8e5c491b15a0
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-21 21:39:20 +0900

    Added other buses

--- backend/src/main/java/org/neatore/wherebus/object/Route.java
+++ backend/src/main/java/org/neatore/wherebus/object/Route.java
@@ -15,10 +15,16 @@ public record Route(String route_id, String route_name, String corpName, String 
         result.put("term", term);
         result.put("length", length);
         result.put("type", switch (type) {
+            case "1" -> "공항버스";
+            case "2" -> "마을버스";
             case "3" -> "간선버스";
             case "4" -> "지선버스";
             case "5" -> "순환버스";
             case "6" -> "광역버스";
+            case "7" -> "인천";
+            case "8" -> "경기";
+            case "9" -> "폐지";
+            case "0" -> "공용";
             default -> null;
         });
         return result;
