edb92bdfc0792df0eef57e4e9c202a6d83c2c0bd
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-21 21:38:41 +0900

    LOGGER Added

--- backend/src/main/java/org/neatore/wherebus/Wherebus.java
+++ backend/src/main/java/org/neatore/wherebus/Wherebus.java
@@ -1,10 +1,13 @@
 package org.neatore.wherebus;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Wherebus {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Wherebus.class);
     public static void main(String[] args) {
         SpringApplication.run(Wherebus.class, args);
     }
