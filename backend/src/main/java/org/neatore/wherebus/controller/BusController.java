333f3676248c136ff6cec934977a9093721880b1
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-21 21:39:38 +0900

    Framing BusController

--- backend/src/main/java/org/neatore/wherebus/controller/BusController.java
+++ backend/src/main/java/org/neatore/wherebus/controller/BusController.java
@@ -1,4 +1,17 @@
 package org.neatore.wherebus.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BusController {
+//    public final BusArrivalService busArrivalService;
+//
+//    public BusController(BusArrivalService busArrivalService) {
+//        this.busArrivalService = busArrivalService;
+//    }
+//
+//    @PostMapping("/getBusInfo")
+//    public ResponseEntity<@NotNull Map<String, Object>> getBusInfo(@RequestParam("route_id") String route_id) {
+//        return ResponseEntity.ok(busArrivalService.getArrivalInformation(route_id));
+//    }
 }
