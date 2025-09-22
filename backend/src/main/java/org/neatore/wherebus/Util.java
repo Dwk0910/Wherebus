e7bf735a4bafc68f58d648b96533afec7866a9ae
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-22 20:54:14 +0900

    Feature: Added Util

--- backend/src/main/java/org/neatore/wherebus/Util.java
+++ backend/src/main/java/org/neatore/wherebus/Util.java
@@ -1,32 +1,58 @@
 package org.neatore.wherebus;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
-import org.neatore.wherebus.object.Route;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class Util {
-//    public static Route getRouteById(String route_id) {
-//        JSONObject data = new JSONObject();
-//
-//        /* Sample Data */
-//        Resource resource = new ClassPathResource("testRouteInfo.json");
-//        try {
-//            byte[] fileData = resource.getInputStream().readAllBytes();
-//            String strData = new String(fileData);
-//            data = new JSONObject(strData);
-//        } catch (IOException ignored) {}
-//
-//        Route result = null;
-//        for (Object o : data.getJSONObject("ServiceResult").getJSONObject("msgBody").getJSONArray("row")) {
-//            JSONObject item = new JSONObject(o.toString());
-//            if (item.getString("RTE_ID").equals(route_id))
-//                result = new Route(item.getString("RTE_ID"), item.getString("RTE_NM"), item.getString("corpNm"), item.getString("stStationNm"), item.getString("edStationNm"), item.getString("term"), item.getString("length"), item.getString("routeType"));
-//        }
-//
-//        if (result == null) throw new IllegalArgumentException("route id " + route_id + " is not a valid route id.");
-//        else return result;
-//    }
+    public static File getFile(File f) {
+        try {
+            if (!f.exists()) Files.createFile(f.toPath());
+            return f;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> T getContent(String fileName, Class<T> returnType) {
+        File f = new File(Wherebus.dataFolder + File.separator + fileName);
+        if (!f.exists()) throw new IllegalArgumentException("File " + fileName + " does not exist.");
+
+        T t = null;
+
+        try {
+            switch (returnType.getSimpleName()) {
+                case "JSONObject" -> {
+                    try {
+                        JSONObject object = new JSONObject(Files.readString(f.toPath()));
+                        t = returnType.cast(object);
+                    } catch (JSONException e) {
+                        throw new IllegalArgumentException("This cannot be parsed to JSONObject: " + e.getMessage());
+                    }
+                }
+
+                case "JSONArray" -> {
+                    try {
+                        JSONArray object = new JSONArray(Files.readString(f.toPath()));
+                        t = returnType.cast(object);
+                    } catch (JSONException e) {
+                        throw new IllegalArgumentException("This cannot be parsed to JSONArray: " + e.getMessage());
+                    }
+                }
+
+                case "String" -> t = returnType.cast(Files.readString(f.toPath()));
+
+                default ->
+                        throw new IllegalArgumentException("return type " + returnType.getSimpleName() + " is not supported.");
+            }
+        } catch (IOException e) {
+            Wherebus.LOGGER.error(e.toString());
+        }
+
+        return t;
+    }
 }
