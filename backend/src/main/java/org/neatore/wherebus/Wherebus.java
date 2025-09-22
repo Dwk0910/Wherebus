a5777623dd46889b913d2d38654f07f20eaf3279
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-22 20:53:49 +0900

    Added datapath system to it

--- backend/src/main/java/org/neatore/wherebus/Wherebus.java
+++ backend/src/main/java/org/neatore/wherebus/Wherebus.java
@@ -5,10 +5,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 @SpringBootApplication
 public class Wherebus {
     public static final Logger LOGGER = LoggerFactory.getLogger(Wherebus.class);
-    public static void main(String[] args) {
+    public static final Path dataFolder = Path.of(System.getProperty("user.dir"), "data");
+
+    public static void main(String[] args) throws IOException {
+        if (!dataFolder.toFile().exists()) Files.createDirectories(dataFolder);
         SpringApplication.run(Wherebus.class, args);
     }
 }
