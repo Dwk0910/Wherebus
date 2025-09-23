856da126d398870ca0ba8e18c33963b2cf7cb841
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-24 00:14:17 +0900

    Fix: Util 정리, backend 이름 바뀐거 적용

--- src/Util.tsx
+++ src/Util.tsx
@@ -1,45 +1,26 @@
 import $ from "jquery";
 
-export interface BasicRouteInfo {
-    corpName: string;
-    route_id: string;
-    route_name: string;
-    length: string;
-    term: string;
-    type: string;
-    start: string;
-    end: string;
-}
-
 export interface Route {
-    corpName: string;
-    route_id: string;
+    id: number;
     route_name: string;
-    length: string;
-    term: string;
+    corpName: string;
     type: string;
+    stations: Array<Station>;
     start: string;
     end: string;
-    arrmsg1: string;
-    arrmsg2: string;
-    arsId: number;
-    busRouteAbrv: number;
-    busRouteId: number;
-    nextBus: string;
-    plainNo1: string;
-    plainNo2: string;
-    stNm: string;
+    length: string;
+    term: string;
 }
 
 export interface Station {
-    stNm: string;
+    stationNm: string;
     arsId: number;
 }
 
-export const getRouteInfoById = async (routeId: string): Promise<Route> => {
+export const getRouteById = async (routeId: string): Promise<Route> => {
     return $.ajax({
         type: "POST",
-        url: "http://localhost:8080/getRouteInfo",
+        url: "http://localhost:8080/getRoute",
         data: { routeId },
         dataType: "json",
     });
