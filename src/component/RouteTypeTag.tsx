e02418d1ed35f691be9e12ef5903663c0b58bee8
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-24 18:02:17 +0900

    RouteTypeTag 정리, types의 전역변수화

--- src/component/RouteTypeTag.tsx
+++ src/component/RouteTypeTag.tsx
@@ -1,44 +1,42 @@
 import { type ReactElement } from "react";
 
-export default function RouteTypeTag({ type }: { type: string }): ReactElement {
-    let str: { bgColor: string; textColor: string; name: string };
-    switch (type) {
-        case "간선버스":
-            str = {
-                bgColor: "#3D5BAB",
-                textColor: "#FFFFFF",
-                name: "간선버스",
-            };
-            break;
-        case "지선버스":
-            str = {
-                bgColor: "#26aa32",
-                textColor: "#FFFFFF",
-                name: "지선버스",
-            };
-            break;
-        case "순환버스":
-            str = {
-                bgColor: "#c57f18",
-                textColor: "#FFFFFF",
-                name: "순환버스",
-            };
-            break;
-        case "광역버스":
-            str = {
-                bgColor: "#f52f08",
-                textColor: "#FFFFFF",
-                name: "광역버스",
-            };
-            break;
-        default:
-            str = {
-                bgColor: "#595959",
-                textColor: "#FFFFFF",
-                name: "알수없음",
-            };
-            break;
+const typeDefs = {
+    "간선버스": {
+        bgColor: "#3D5BAB",
+        textColor: "#FFFFFF",
+        name: "간선버스",
+    },
+    "지선버스": {
+        bgColor: "#26aa32",
+        textColor: "#FFFFFF",
+        name: "지선버스",
+    },
+    "순환버스": {
+        bgColor: "#c57f18",
+        textColor: "#FFFFFF",
+        name: "순환버스",
+    },
+    "광역버스": {
+        bgColor: "#f52f08",
+        textColor: "#FFFFFF",
+        name: "광역버스",
+    },
+    "알수없음": {
+        bgColor: "#595959",
+        textColor: "#FFFFFF",
+        name: "알수없음",
     }
+}
+
+export const getColor = (type: string): string => {
+    let str = typeDefs[type];
+    if (str === null) str = typeDefs["알수없음"];
+    return str.bgColor;
+}
+
+export default function RouteTypeTag({ type }: { type: string }): ReactElement {
+    let str = typeDefs[type];
+    if (str === null) str = typeDefs["알수없음"];
 
     return (
         <span
