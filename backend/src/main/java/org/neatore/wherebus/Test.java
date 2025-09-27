f07fc7cb1323c39a87a3aca30f9ad7393a1a194b
Dongwan Kim <66874914+Dwk0910@users.noreply.github.com>
2025-09-27 21:43:40 +0900

    Test: Calling API

--- backend/src/main/java/org/neatore/wherebus/Test.java
+++ backend/src/main/java/org/neatore/wherebus/Test.java
@@ -1,19 +1,25 @@
 package org.neatore.wherebus;
 
-import org.json.JSONObject;
-import org.json.XML;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
 
 public class Test {
     public static void main(String[] args) {
-        Path p = Path.of(Wherebus.dataFolder.toString(), "testData.xml");
         try {
-            JSONObject obj = XML.toJSONObject(Files.readString(p));
-            System.out.println(obj.toString(4));
-        } catch (IOException e) {
+            URL url = new URI("http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?serviceKey=%s&routeId=%s".formatted(System.getenv("WHEREBUS_APIKEY_DATAGOKR"), "100100001")).toURL();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))) {
+                char[] buffer = new char[16384];
+                int bytesRead;
+                StringBuilder response = new StringBuilder();
+                while ((bytesRead = reader.read(buffer)) != -1) {
+                    response.append(buffer, 0, bytesRead);
+                }
+
+                System.out.println(response);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
