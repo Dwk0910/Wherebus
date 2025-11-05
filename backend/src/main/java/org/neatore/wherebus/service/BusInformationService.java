da8eae8d8ca9ba8415e6aded16a7be42b6e33d80
neatore <66874914+Dwk0910@users.noreply.github.com>
2025-11-05 23:08:42 +0900

    Feature: Added live bus refreshing system

--- backend/src/main/java/org/neatore/wherebus/service/BusInformationService.java
+++ backend/src/main/java/org/neatore/wherebus/service/BusInformationService.java
@@ -1,7 +1,6 @@
 package org.neatore.wherebus.service;
 
 import org.neatore.wherebus.Wherebus;
-import org.neatore.wherebus.Util;
 
 import org.springframework.stereotype.Service;
 
@@ -10,51 +9,65 @@ import org.json.JSONObject;
 import org.json.XML;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Map;
 
 @Service
 public class BusInformationService {
     public Map<String, Object> getBuses(String route_id) {
-        Map<String, Object> buses = new HashMap<>();
-        try {
-//            URL url = new URI("http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?serviceKey=%s&routeId=%s".formatted(System.getenv("WHEREBUS_APIKEY_DATAGOKR"), route_id)).toURL();
+//        try {
+//            // TODO: Change API to http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt
+//            URL url = new URI("http://ws.bus.go.kr/api/rest/arrive/getArrInfoByRouteAll?serviceKey=%s&busRouteId=%s".formatted(System.getenv("WHEREBUS_APIKEY_DATAGOKR"), route_id)).toURL();
 //            try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openConnection().getInputStream()))) {
-            File f = Path.of(Wherebus.dataFolder.toString(), "test_arrival.xml").toFile();
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(Files.newInputStream(f.toPath())))) {
-                JSONObject response = XML.toJSONObject(reader);
-                JSONArray stations = response.getJSONObject("ServiceResult").getJSONObject("msgBody").getJSONArray("itemList");
-                for (Object o : stations) {
-                    JSONObject station = new JSONObject(o.toString());
-                    if (!Util.isEmpty(station.getString("plainNo1"))) {
-                        Map<String, Object> newMap = new HashMap<>();
-                        newMap.put("id", station.get("vehId1"));
-                        newMap.put("plate", station.get("plainNo1"));
-                        newMap.put("bustype", station.get("busType1"));
-                        newMap.put("currentstop", station.get("stationNm1"));
-                        newMap.put("inftype", station.get("rerdie_Div1"));
-                        newMap.put("inf", station.get("reride_Num1"));
-                        newMap.put("islast", Integer.parseInt(station.get("isLast1").toString()) == 1);
+//                JSONObject response = XML.toJSONObject(reader);
+//                JSONArray stations = response.getJSONObject("ServiceResult").getJSONObject("msgBody").getJSONArray("itemList");
+//                for (Object o : stations) {
+//                    JSONObject station = new JSONObject(o.toString());
+//                    if (!Util.isEmpty(station.getString("plainNo1"))) {
+//                        Map<String, Object> newMap = new HashMap<>();
+//                        newMap.put("id", station.get("vehId1"));
+//                        newMap.put("plate", station.get("plainNo1"));
+//                        newMap.put("bustype", station.get("busType1"));
+//                        newMap.put("nextstop", station.get("nstnId1"));
+//                        newMap.put("inftype", station.get("rerdie_Div1"));
+//                        newMap.put("inf", station.get("reride_Num1"));
+//                        newMap.put("islast", Integer.parseInt(station.get("isLast1").toString()) == 1);
+//                        newMap.put("isarrive", station.get("isArrive1"));
+//
+//                        buses.put(station.get("plainNo1").toString(), newMap);
+//                    }
+//                }
+//            }
+//        } catch (IOException | URISyntaxException e) {
+//            Wherebus.LOGGER.error(e.toString());
+//        }
+        JSONObject response = getBuses_(route_id);
+        return response.toMap();
+    }
 
-                        buses.put(station.get("plainNo1").toString(), newMap);
-                    }
-                }
+    private JSONObject getBuses_(String routeId) {
+        final JSONObject result = new JSONObject();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new URI("http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey=%s&busRouteId=%s".formatted(System.getenv("WHEREBUS_APIKEY_DATAGOKR"), routeId)).toURL().openConnection().getInputStream()))) {
+            JSONObject response_ = XML.toJSONObject(reader);
+            JSONArray response = response_.getJSONObject("ServiceResult").getJSONObject("msgBody").getJSONArray("itemList");
+            for (Object o : response) {
+                JSONObject bus = new JSONObject(o.toString());
+                JSONObject generated = new JSONObject();
+                generated.put("id", bus.get("vehId"));
+                generated.put("plate", bus.get("plainNo"));
+                generated.put("bustype", bus.get("busType"));
+                generated.put("nextStop", bus.get("nextStId"));
+                generated.put("sectpos", (bus.getInt("sectOrd") - 1) + (bus.get("stopFlag").toString().equals("0") ? ".5" : ""));
+                result.put(generated.get("id").toString(), generated);
             }
-//        } catch (URISyntaxException | IOException e) {
-        } catch (IOException e) {
+        } catch (URISyntaxException | IOException e) {
             Wherebus.LOGGER.error(e.toString());
         }
-
-        return buses;
+        return result;
     }
 }
--- src/pages/routes/ViewRoutes.tsx
+++ src/pages/routes/ViewRoutes.tsx
@@ -1,10 +1,14 @@
 import { type JSX, type ReactNode, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
+import { clsx } from "clsx";
+
 import { type Route, type Station, getRouteById, getBus } from "../../Util";
 
 import RouteTypeTag, { getColor } from "../../component/RouteTypeTag";
 
+import { IoIosRefresh } from "react-icons/io";
+import { MdOutlineUTurnLeft } from "react-icons/md";
 import { IoCaretDownCircleOutline } from "react-icons/io5";
 import { FaBus } from "react-icons/fa";
 
@@ -19,6 +23,31 @@ export default function ViewRoutes() {
     const [route, setRoute] = useState<Route | null | undefined>(null);
     const [stations, setStations] = useState<Array<Station>>([]);
     const [buses, setBuses] = useState<Array<Bus>>([]);
+    const [lastRefresh, setLastRefresh] = useState<string>("");
+
+    const refreshBus = async () => {
+        await getBus(routeId as string).then((data: Object) => {
+            let busList: Array<Bus> = [];
+            for (const [_, v] of Object.entries(data)) {
+                busList.push({
+                    pos: v["sectpos"],
+                    plate: v["plate"],
+                });
+            }
+
+            setBuses(busList);
+
+            const date_formatter = new Intl.DateTimeFormat("ko-KR", {
+                hour: "2-digit",
+                minute: "2-digit",
+                second: "2-digit",
+                hour12: false,
+                timeZone: "Asia/Seoul",
+            });
+
+            setLastRefresh(date_formatter.format(Date.now()));
+        });
+    };
 
     const getBusElement = (idx: string): JSX.Element | "" => {
         for (const bus of buses) {
@@ -41,27 +70,18 @@ export default function ViewRoutes() {
 
     useEffect(() => {
         // Register route info
-        getRouteById(routeId as string).then((res) => {
+        getRouteById(routeId as string).then(async (res) => {
             if (res === null) setRoute(undefined);
             else {
                 setRoute(res);
                 setStations(res.stations);
             }
+
+            await refreshBus();
         });
 
-        // TODO: Scheduler for live bus info
-        getBus(routeId as string).then((res) => {
-            console.log(res);
-        });
-
-        // **TEST**
-        // setBuses([{
-        //     pos: "0.5",
-        //     plate: "서울74아1275"
-        // }, {
-        //     pos: "5",
-        //     plate: "서울74사2577"
-        // }])
+        const interval_refresh = setInterval(refreshBus, 10000);
+        return () => clearInterval(interval_refresh);
     }, [routeId]);
 
     let content: ReactNode;
@@ -72,12 +92,30 @@ export default function ViewRoutes() {
             <div className={"w-full flex flex-col px-4"}>
                 <div
                     className={
-                        "w-full h-15 mt-1 pl-5 flex flex-row justify-start items-center pt-13 pb-10"
+                        "w-full h-15 mt-1 pl-5 flex flex-row justify-start items-center pt-13 pb-7"
                     }
                 >
                     <RouteTypeTag type={route.type} />
                     <span className={"font-suite text-4xl ml-3"}>
                         {route.route_name}
+                    </span>
+                </div>
+                <div className={"flex flex-row ml-5 pb-1"}>
+                    <span className={"font-suite text-[1.2rem] text-gray-300"}>
+                        마지막 새로고침
+                    </span>
+                    <span
+                        className={"mt-1 ml-2 text-[1.2rem] cursor-pointer"}
+                        onClick={async () => {
+                            await refreshBus();
+                        }}
+                    >
+                        <IoIosRefresh />
+                    </span>
+                    <span
+                        className={"font-SeoulNamsan text-[1.2rem] ml-3 mt-0.5"}
+                    >
+                        {lastRefresh}
                     </span>
                 </div>
                 <div
@@ -159,7 +197,7 @@ export default function ViewRoutes() {
                                         className={"flex flex-col items-start"}
                                     >
                                         <div className={"flex flex-row"}>
-                                            {/*Bus (Station) Layer*/}
+                                            {/*Bus (Station & stopped bus element) Layer*/}
                                             <div
                                                 className={
                                                     "absolute flex flex-col justify-start items-center w-10 mt-0.5"
@@ -173,22 +211,53 @@ export default function ViewRoutes() {
                                             />
                                         </div>
                                         <div className={"flex flex-row"}>
-                                            {/*Bus (Line) Layer*/}
+                                            {/*Bus (Line & moving bus element) Layer*/}
                                             <div
-                                                className={
-                                                    "absolute flex flex-col justify-start items-center w-10 mt-[12px]"
-                                                }
+                                                className={clsx(
+                                                    "absolute flex flex-col justify-start items-center w-10",
+                                                    item.transYn === "Y"
+                                                        ? "mt-[40px]"
+                                                        : "mt-[12px]"
+                                                )}
                                             >
                                                 {getBusElement(
                                                     idx.toString() + ".5"
                                                 )}
                                             </div>
+
                                             {idx < stations.length - 1 ? (
                                                 <div
-                                                    className={
-                                                        "w-1.5 h-11 bg-gray-400 mt-[-2px] mb-[-5px] ml-13"
-                                                    }
-                                                ></div>
+                                                    className={clsx(
+                                                        "w-1.5 bg-gray-400 mt-[-2px] mb-[-5px] ml-13 relative",
+                                                        item.transYn === "Y"
+                                                            ? "h-25"
+                                                            : "h-11"
+                                                    )}
+                                                >
+                                                    <div
+                                                        className={
+                                                            "bg-white absolute w-15 text-black ml-[-10px] mt-10 rounded-[15px] font-suite"
+                                                        }
+                                                    >
+                                                        {item.transYn ===
+                                                        "Y" ? (
+                                                            <div
+                                                                className={
+                                                                    "flex flex-row items-center justify-center"
+                                                                }
+                                                            >
+                                                                <MdOutlineUTurnLeft
+                                                                    className={
+                                                                        "ml-[-4px]"
+                                                                    }
+                                                                />
+                                                                회차
+                                                            </div>
+                                                        ) : (
+                                                            ""
+                                                        )}
+                                                    </div>
+                                                </div>
                                             ) : (
                                                 <div
                                                     className={
@@ -200,7 +269,14 @@ export default function ViewRoutes() {
                                     </div>
 
                                     {/*Content Layer*/}
-                                    <div className={"h-15 ml-3 flex flex-col"}>
+                                    <div
+                                        className={clsx(
+                                            "ml-3 flex flex-col",
+                                            item.transYn === "Y"
+                                                ? "h-28.5"
+                                                : "h-15"
+                                        )}
+                                    >
                                         <span
                                             className={
                                                 "font-SeoulNamsan font-bold text-nowrap max-w-50"
